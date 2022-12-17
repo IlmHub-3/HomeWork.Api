0462b907bb5363f3ea54aee2f781a759f5d6872f
Botirjon29 <110552031+Botirjon29@users.noreply.github.com>
2022-12-18 04:43:32 +0500

    add generic repository

---
+++ HomeWork.Data/Repository/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace HomeWork.Data.Repository;
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    TEntity? GetById(int id);
+    TEntity? GetById(Guid id);
+    IQueryable<TEntity> GetAll();
+    Task<TEntity> AddAsync(TEntity entity);
+    Task<TEntity> Remove(TEntity entity);
+    Task<TEntity> Update(TEntity entity);
+}
