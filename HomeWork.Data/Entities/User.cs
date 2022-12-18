3f19e92a9adc010fa9b1a99f8116582e982536ae
Botirjon29 <110552031+Botirjon29@users.noreply.github.com>
2022-12-18 09:34:02 +0500

    changes

--- HomeWork.Data/Entities/User.cs
+++ HomeWork.Data/Entities/User.cs
@@ -7,6 +7,6 @@ public class User : IdentityUser<Guid>
     public string? Firstname { get; set; }
     public string? Lastname { get; set; }
 
-    public virtual List<UserGroup>? Courses { get; set; }
+    public virtual List<UserGroup>? Groups { get; set; }
     public virtual List<UserTask>? UserTasks { get; set; }
 }
--- HomeWork.Data/Entities/UserGroup.cs
+++ HomeWork.Data/Entities/UserGroup.cs
@@ -6,8 +6,6 @@ public class UserGroup
 {
     public Guid Id { get; set; }
 
-    public User? Student { get; set; }
-    public User? Teacher { get; set; }
     public Guid UserId { get; set; }
     [ForeignKey(nameof(UserId))]
     public virtual User? User { get; set; }
