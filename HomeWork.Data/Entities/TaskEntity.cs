3f9b089d7c2978bf20429330f85f51e02eca25b4
abuUsman_04 <i123l183l@gmail.com>
2022-12-18 09:33:23 +0300

    entities modified, added some changes

--- HomeWork.Data/Entities/TaskEntity.cs
+++ HomeWork.Data/Entities/TaskEntity.cs
@@ -8,13 +8,12 @@ public class TaskEntity
     public Guid Id { get; set; }
     public string? Title { get; set; }
     public string? Comment { get; set; }
-    public List<string>? TaskFayl { get; set; }
-    public List<string>? ResultFayl { get; set; }
+    public List<string>? TaskFile { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime? EndDate { get; set; }
     public DateTime? ResultLoadedDate { get; set; }
     public ETaskStatus Status { get; set; }
-
+    public string? Description { get; set; }
     public Guid GroupId { get; set; }
     [ForeignKey(nameof(GroupId))]
     public virtual Group? Course { get; set; }
