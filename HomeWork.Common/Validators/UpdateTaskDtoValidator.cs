c8e8df8a7dc6f4a489dfe45dbda705d6032ff2bf
Botirjon29 <110552031+Botirjon29@users.noreply.github.com>
2022-12-18 19:46:36 +0500

    added validator

---
+++ HomeWork.Common/Validators/UpdateTaskDtoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using HomeWork.BLL.Dtos;
+
+namespace HomeWork.Common.Validators;
+public class UpdateTaskDtoValidator : AbstractValidator<UpdateTaskDto>
+{
+    public UpdateTaskDtoValidator()
+    {
+        RuleFor(updateDto => updateDto.Title).NotNull();
+        RuleFor(updateDto => updateDto.Title).Length(3, 30).When(updateDto => updateDto.Title != null);
+    }
+}
